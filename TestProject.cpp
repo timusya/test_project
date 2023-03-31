2caaf70b99dc718af98c3925ed8fdd446691697f
Tim <turganovtim@gmail.com>
2023-03-31 21:54:07 +0500

    another commit

---
+++ TestProject.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+    cout << sum << endl;
+    return 0;
+}
